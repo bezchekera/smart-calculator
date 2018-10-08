d532cf7022870b86e87a2872e74850e9ae85be85
bezchekera <aniaruyceva@mail.ru>
2018-10-08 16:28:58 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,54 +1,73 @@
-class SmartCalculator {
-  constructor(initialValue) {
-  this.value=0;// your implementation
-  this.num1=initialValue;
-  this.num2=initialValue;
-
-  }
-
-  add(number) {
-    this.value += this.num1;
-    this.num1 = number;
-    this.num2 = number;
-    return this.value+this.num1;
-   //return this;
-    // your implementation
-  }
+class SmartCalculator { 
+    constructor(initialValue) { 
+    this.value=0;
+    this.num1=initialValue; 
+    this.num2=initialValue; 
+    this.p=0;
+    this.step;
+    this.osnov;
+    this.zna=[];
+    } 
+    
+    add(number) { 
+    this.value += this.num1; 
+    this.num1 = number; 
+    this.num2 = number; 
+    this.p=0;
+    //console.log(this.value+this.num1);
+    //return this.value+this.num1; 
+    return this; 
+    
+    } 
+    
+    subtract(number) { 
+    this.value += this.num1; 
+    this.num1 = number*(-1); 
+    this.num2 = number; 
+    this.p=0;
+    //return this.value+this.num1; 
+    //console.log(this.value+this.num1);
+    return this; 
+    
+    } 
+    
+    multiply(number) { 
+    this.num1 = this.num1*number; 
+    this.num2 = number; 
+    this.p=0;
+    //console.log(this.value+this.num1);
+    return this; 
+     //return this.value+this.num1; 
   
-  subtract(number) {
-    this.value += this.num1;
-    this.num1 = number*(-1);
-    this.num2 = number;
-    //return this.value+this.num1;
-    return this;
-    // your implementation
-  }
-
-  multiply(number) {
-    this.num1 = this.num1*number;
-    this.num2 = number;
-    return this;
-    //return this.value+this.num1;
-    // your implementation
-  }
-
-  devide(number) {
-    if (this.num1>0) {    this.num1 = Math.floor(this.num1/number);}
-    if (this.num1>0) {    this.num1 = Math.ceil(this.num1/number);}
-    this.num2 = number;
-    return this;
-  //return this.value+this.num1;
-    // your implementation
-  }
-
-  pow(number) {
+    } 
+    
+    devide(number) { 
+   this.num1 = Math.floor(this.num1/number);
+    //if (this.num1>0) { this.num1 = Math.ceil(this.num1/number);} 
+    this.num2 = number; 
+    this.p=0;
+    //console.log(this.value+this.num1);
+    return this; 
+    
+     //return this.value+this.num1; 
+   
+    } 
+    
+    pow(number) { 
+   //     if  (this.p=0) {
     this.num1 = Math.floor(this.num1/this.num2)*Math.pow( this.num2, number);
-    //return this.value+this.num1;
-    return this;
-    // your implementation
-  }
- 
-  
-}
-
-module.exports = SmartCalculator;
+    //this.step=this.num2; 
+    //this.osnov=Math.floor(this.num1/this.num2)}
+   
+   // this.p= this.p+1;
+    
+    return this; 
+    // your implementation 
+    } 
+    
+    toString() {
+        return this.value+this.num1;
+    }
+    } 
+    
+    module.exports = SmartCalculator;
